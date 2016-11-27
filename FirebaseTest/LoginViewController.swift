a0160b5a453e4c13897f8593e8841829df2e6315
Rajesh M <rajeshm79@outlook.com>
2016-11-27 07:36:58 -0800

    commit

--- FirebaseTest/LoginViewController.swift
+++ FirebaseTest/LoginViewController.swift
@@ -7,11 +7,23 @@
 //
 
 import UIKit
+import FirebaseAuth
+
 
 class LoginViewController: UIViewController {
     
     
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
     let gradientLayer = CAGradientLayer()
+    
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if let user = FIRAuth.auth()?.currentUser {
+            self.signedIn(user)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,14 +35,14 @@ class LoginViewController: UIViewController {
         gradientLayer.frame = self.view.bounds
         
         // 3
-        let color1 = UIColor.yellowColor().CGColor as CGColorRef
-        let color2 = UIColor(red: 1.0, green: 0, blue: 0, alpha: 1.0).CGColor as CGColorRef
-        let color3 = UIColor.clearColor().CGColor as CGColorRef
+        let color1 = UIColor.magentaColor().CGColor as CGColorRef
+        let color2 = UIColor(red: 244.0/255, green: 200.2/255, blue: 120.2/255, alpha: 0.0).CGColor as CGColorRef
+        let color3 = UIColor.greenColor().CGColor as CGColorRef
         let color4 = UIColor(white: 0.0, alpha: 0.7).CGColor as CGColorRef
-        gradientLayer.colors = [color1, color2, color3, color4]
+        gradientLayer.colors = [color2,color1, color3, color4]
         
         // 4
-        gradientLayer.locations = [0.0, 0.25, 0.75, 1.0]
+        gradientLayer.locations = [0.002, 0.25, 0.75, 1.0]
         
         // 5
         self.view.layer.addSublayer(gradientLayer)
@@ -38,6 +50,84 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func didTapSignIn(sender: AnyObject) {
+        
+        let appdelegate:AppDelegate = AppDelegate()
+        
+        // Sign In with credentials.
+        guard let email = emailField.text, let password = passwordField.text else { return }
+      FIRAuth.auth()?.signIn(withEmail: email, password: password) { (user, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            self.signedIn(user!)
+        }
+
+    }
+    
+    
+    
+    
+    
+    @IBAction func didTapSignUp(sender: AnyObject) {
+        
+        guard let email = emailField.text, let password = passwordField.text else { return }
+        FIRAuth.auth()?.createUser(withEmail: email, password: password) { (user, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            self.setDisplayName(user!)
+        }
+
+    }
+    
+    func setDisplayName(_ user: FIRUser) {
+        let changeRequest = user.profileChangeRequest()
+        changeRequest.displayName = user.email!.components(separatedBy: "@")[0]
+        changeRequest.commitChanges(){ (error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            self.signedIn(FIRAuth.auth()?.currentUser)
+        }
+    }
+
+    @IBAction func didRequestPasswordReset(_ sender: AnyObject) {
+        let prompt = UIAlertController.init(title: nil, message: "Email:", preferredStyle: .alert)
+        let okAction = UIAlertAction.init(title: "OK", style: .default) { (action) in
+            let userInput = prompt.textFields![0].text
+            if (userInput!.isEmpty) {
+                return
+            }
+            FIRAuth.auth()?.sendPasswordReset(withEmail: userInput!) { (error) in
+                if let error = error {
+                    print(error.localizedDescription)
+                    return
+                }
+            }
+        }
+        prompt.addTextField(configurationHandler: nil)
+        prompt.addAction(okAction)
+        present(prompt, animated: true, completion: nil);
+    }
+
+    
+    
+    func signedIn(_ user: FIRUser?) {
+        MeasurementHelper.sendLoginEvent()
+        
+        AppState.sharedInstance.displayName = user?.displayName ?? user?.email
+        AppState.sharedInstance.photoURL = user?.photoURL
+        AppState.sharedInstance.signedIn = true
+        let notificationName = Notification.Name(rawValue: Constants.NotificationKeys.SignedIn)
+        NotificationCenter.default.post(name: notificationName, object: nil, userInfo: nil)
+        performSegue(withIdentifier: Constants.Segues.SignInToFp, sender: nil)
+        }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
